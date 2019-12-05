3883e6a6de902fc031919ebbef8978999d775bf3
Fariz HA <hasabifariz@gmail.com>
2019-12-05 09:10:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/logincrud/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.logincrud.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.logincrud.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
